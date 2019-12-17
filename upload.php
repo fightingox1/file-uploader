da1643b8df184b0f9a6e8e9766833bcba6234ed7
fightingox1 <44482532+fightingox1@users.noreply.github.com>
2019-12-17 09:42:06 -0500

    Update upload.php

--- upload.php
+++ upload.php
@@ -15,3 +15,17 @@ if(isset($_POST["submit"])) {
     }
 }
 ?>
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
